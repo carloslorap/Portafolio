4b9110c1749b5ff97e21dddc2eed4e218528a6ba
carloslorap <74977007@certus.edu.pe>
2023-06-05 08:50:50 -0500

    erghregh

---
+++ main.js
@@ -0,0 +1,67 @@
+var typed =new Typed('.typing',{
+    strings:["Web Designer","Web Developer","Graphics Designer"],
+    typeSpeed:100,
+    BackSpeed:9000,
+    loop:true
+})
+
+
+
+///aside
+
+const nav =document.querySelector(".nav"),
+      navList= nav.querySelectorAll("li"),
+      totalNavList=navList.length,
+      allsection=document.querySelectorAll(".section"),
+      totalSection=allsection.length;
+      for(let i =0 ;i < totalNavList;i++){
+        const a=navList[i].querySelector("a");
+        a.addEventListener("click",function(){
+            for (let i = 0; i < totalSection; i++) {
+                allsection[i].classList.remove("back-section");
+                
+            }
+            for(let j = 0 ;j < totalNavList ;j++){
+                if (navList[j].querySelector("a").classList.contains("active")) {
+                    allsection[j].classList.add("back-section");
+                }
+                navList[j].querySelector("a").classList.remove("active");
+            }
+            this.classList.add("active")
+            showSection(this);
+
+            //esto de aca cuando estes en responsi	y este abierto el menu lateral y machuques en algunas de esos apartado automaticamente se cerrara el menu lateral
+            if (window.innerWidth<1200  ) {
+                asideSectionTogglerBtn();
+            }
+
+        })
+      }
+     
+      function showSection(element){
+        for (let i = 0; i < totalSection; i++) {
+            allsection[i].classList.remove("active");
+            
+        }
+        const target =element.getAttribute("href").split("#")[1];
+        document.querySelector("#"+target).classList.add("active")
+      }
+
+      const navtogglerBtn=document.querySelector(".nav-toggler"),
+            aside=document.querySelector(".aside");
+            navtogglerBtn.addEventListener("click",()=>{
+                asideSectionTogglerBtn();
+            })
+            function asideSectionTogglerBtn(){
+                aside.classList.toggle("open");
+                navtogglerBtn.classList.toggle("open");
+
+                //este for es para q cuando estes en responsi y abras el menu lateral hace que las section no rompa la barrera y q respete el recuadro (si quieres elimina este for y te daras cuenta)
+                for (let i = 0; i < totalSection; i++) {
+                    allsection[i].classList.toggle("open");
+                    
+                }
+            }
+
+            
+  
---
+++ style-switcher.js
@@ -0,0 +1,41 @@
+// toggle de la parte de los colores su contenedor para q cuando des un click se visualize el cuadro (start)
+
+const styleSwitcherToggle = document.querySelector('.style-switcher-toggler') ;
+styleSwitcherToggle.addEventListener('click',()=>{
+    document.querySelector('.style-switcher').classList.toggle('open'); //el open es un codigo q lo creaste en el css no en el html
+})
+
+//(end)
+
+// esta parte es cuando el usuario haga un scroll osea baje ,el contenedor se cerrara automaticamente(start)
+window.addEventListener('scroll',()=>{
+    if(document.querySelector('.style-switcher').classList.contains('open')){
+        document.querySelector('.style-switcher').classList.remove('open')
+    }
+})
+//(end)
+
+const alternateStyle = document.querySelectorAll('.alternate-style');
+function setActiveStyle(color){
+    alternateStyle.forEach((style)=>{
+        if (color === style.getAttribute('title')) {
+            style.removeAttribute('disabled');
+        }else{
+            style.setAttribute('disabled','true');
+        }
+    })
+}
+
+const dayNight= document.querySelector('.day-night');
+dayNight.addEventListener('click',()=>{
+    dayNight.querySelector('i').classList.toggle('fa-sun'); 
+    dayNight.querySelector('i').classList.toggle('fa-moon'); 
+    document.body.classList.toggle('dark');
+})
+window.addEventListener('load',()=>{
+    if (document.body.classList.contains('dark')) {
+        dayNight.querySelector('i').classList.add('fa-sun'); 
+    }else{
+        dayNight.querySelector('i').classList.add('fa-moon'); 
+    }
+})
