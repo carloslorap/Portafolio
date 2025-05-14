a2d9ab467f5340e33a52c910c5c6c0e7dc2f9586
carloslorap <81603256+carloslorap@users.noreply.github.com>
2025-05-14 18:02:04 -0500

    feat: agregando descarga de CV y redirecionando boton de contactame a la seccion de contacto

--- main.js
+++ main.js
@@ -1,100 +1,99 @@
-var typed =new Typed('.typing',{
-    strings:["Diseñador web","Desarrollador web","Diseñador gráfico"],
-    typeSpeed:100,
-    BackSpeed:9000,
-    loop:true
-})
-
-
+var typed = new Typed(".typing", {
+  strings: ["Diseñador web", "Desarrollador web", "Diseñador gráfico"],
+  typeSpeed: 100,
+  BackSpeed: 9000,
+  loop: true,
+});
 
 ///aside
 
-const nav =document.querySelector(".nav"),
-      navList= nav.querySelectorAll("li"),
-      totalNavList=navList.length,
-      allsection=document.querySelectorAll(".section"),
-      totalSection=allsection.length;
-      for(let i =0 ;i < totalNavList;i++){
-        const a=navList[i].querySelector("a");
-        a.addEventListener("click",function(){
-            for (let i = 0; i < totalSection; i++) {
-                allsection[i].classList.remove("back-section");
-                
-            }
-            for(let j = 0 ;j < totalNavList ;j++){
-                if (navList[j].querySelector("a").classList.contains("active")) {
-                    allsection[j].classList.add("back-section");
-                }
-                navList[j].querySelector("a").classList.remove("active");
-            }
-            this.classList.add("active")
-            showSection(this);
-
-            //esto de aca cuando estes en responsi	y este abierto el menu lateral y machuques en algunas de esos apartado automaticamente se cerrara el menu lateral
-            if (window.innerWidth<1200  ) {
-                asideSectionTogglerBtn();
-            }
-
-        })
-      }
-     
-      function showSection(element){
-        for (let i = 0; i < totalSection; i++) {
-            allsection[i].classList.remove("active");
-            
-        }
-        const target =element.getAttribute("href").split("#")[1];
-        document.querySelector("#"+target).classList.add("active")
-      }
-
-      const navtogglerBtn=document.querySelector(".nav-toggler"),
-            aside=document.querySelector(".aside");
-            navtogglerBtn.addEventListener("click",()=>{
-                asideSectionTogglerBtn();
-            })
-            function asideSectionTogglerBtn(){
-                aside.classList.toggle("open");
-                navtogglerBtn.classList.toggle("open");
-
-                //este for es para q cuando estes en responsi y abras el menu lateral hace que las section no rompa la barrera y q respete el recuadro (si quieres elimina este for y te daras cuenta)
-                for (let i = 0; i < totalSection; i++) {
-                    allsection[i].classList.toggle("open");
-                    
-                }
-            }
-
-            
-  
-
-      
-
-            const x={foo:1};
-            const y={foo:2};
-
-
-
-    //proceso para sacar automaticamente la edad        
-    const yearActual = new Date().getFullYear()
-    const mesActual = new Date().getMonth()
-    const diaActual = new Date().getDate()
-
-    const restafinal = yearActual - 2003
-
-    const funcionEdad=()=>{
-        if (mesActual < 1 || (mesActual === 1 && diaActual < 11)) {
-           return restafinal - 1
-         }
-
-         return restafinal
-        
+const nav = document.querySelector(".nav"),
+  navList = nav.querySelectorAll("li"),
+  totalNavList = navList.length,
+  allsection = document.querySelectorAll(".section"),
+  totalSection = allsection.length;
+for (let i = 0; i < totalNavList; i++) {
+  const a = navList[i].querySelector("a");
+  a.addEventListener("click", function () {
+    for (let i = 0; i < totalSection; i++) {
+      allsection[i].classList.remove("back-section");
     }
-    document.getElementById('age').textContent = funcionEdad();
+    for (let j = 0; j < totalNavList; j++) {
+      if (navList[j].querySelector("a").classList.contains("active")) {
+        allsection[j].classList.add("back-section");
+      }
+      navList[j].querySelector("a").classList.remove("active");
+    }
+    this.classList.add("active");
+    showSection(this);
 
+    //esto de aca cuando estes en responsi	y este abierto el menu lateral y machuques en algunas de esos apartado automaticamente se cerrara el menu lateral
+    if (window.innerWidth < 1200) {
+      asideSectionTogglerBtn();
+    }
+  });
+}
 
-   
+function showSection(element) {
+  for (let i = 0; i < totalSection; i++) {
+    allsection[i].classList.remove("active");
+  }
+  const target = element.getAttribute("href").split("#")[1];
+  document.querySelector("#" + target).classList.add("active");
+}
 
+function updateNav(element) {
+  for (let i = 0; i < totalNavList; i++) {
+    navList[i].querySelector("a").classList.remove("active");
+    const target = element.getAttribute("href").split("#")[1];
+    console.log(target);
+    
+    if (
+      target ==
+      navList[i].querySelector("a").getAttribute("href").split("#")[1]
+    ) {
+      navList[i].querySelector("a").classList.add("active");
+    }
+  }
+}
 
-            
-         
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("#hire-me").addEventListener("click", function () {
+    showSection(this);
+    updateNav(this);
+  });
+});
 
-       
+const navtogglerBtn = document.querySelector(".nav-toggler"),
+  aside = document.querySelector(".aside");
+navtogglerBtn.addEventListener("click", () => {
+  asideSectionTogglerBtn();
+});
+function asideSectionTogglerBtn() {
+  aside.classList.toggle("open");
+  navtogglerBtn.classList.toggle("open");
+
+  //este for es para q cuando estes en responsi y abras el menu lateral hace que las section no rompa la barrera y q respete el recuadro (si quieres elimina este for y te daras cuenta)
+  for (let i = 0; i < totalSection; i++) {
+    allsection[i].classList.toggle("open");
+  }
+}
+
+const x = { foo: 1 };
+const y = { foo: 2 };
+
+//proceso para sacar automaticamente la edad
+const yearActual = new Date().getFullYear();
+const mesActual = new Date().getMonth();
+const diaActual = new Date().getDate();
+
+const restafinal = yearActual - 2003;
+
+const funcionEdad = () => {
+  if (mesActual < 1 || (mesActual === 1 && diaActual < 11)) {
+    return restafinal - 1;
+  }
+
+  return restafinal;
+};
+document.getElementById("age").textContent = funcionEdad();
